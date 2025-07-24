4c5bf9e79ae2e44202de7d2f6f6b3bb9879c64ff
Aman Arora <145247273+aroraaman2105@users.noreply.github.com>
2025-07-25 02:29:36 +0530

    Add files via upload

---
+++ E-commerce Site/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+       <div className="header-contents">
+        <h2>Craving something delicious?</h2>
+        <p>From spicy street food to gourmet meals, explore a world of flavours made fresh by local chefs and delivered hot to your door. Whether it’s a midnight snack or a family feast, we’ve got you covered with speed, quality, and taste.</p>
+        <button>View Menu</button>
+       </div>
+    </div>
+  )
+}
+
+export default Header
